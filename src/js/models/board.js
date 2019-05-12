f26a0198f397b4e271ff374294a259357a989cbc
Stefan Fortuin <stefanfortuin02@gmail.com>
2019-05-12 18:42:37 +0200

    generate config

--- src/js/models/board.js
+++ src/js/models/board.js
@@ -1,13 +1,45 @@
 import die from './die'
 
+const dices =  [
+	["R", "I", "F", "O", "B", "X"], //1
+	["I", "F", "E", "H", "E", "Y"], //2
+	["D", "E", "N", "O", "W", "S"], //3
+	["U", "T", "O", "K", "N", "D"], //4
+	["H", "M", "S", "R", "A", "O"], //5
+	["L", "U", "P", "E", "T", "S"], //6
+	["A", "C", "I", "T", "O", "A"], //7
+	["Y", "L", "G", "K", "U", "E"], //8
+	["Q", "B", "M", "J", "O", "A"], //9
+	["E", "H", "I", "S", "P", "N"], //10
+	["V", "E", "T", "I", "G", "N"], //11
+	["B", "A", "L", "I", "Y", "T"], //12
+	["E", "Z", "A", "V", "N", "D"], //13
+	["R", "A", "L", "E", "S", "C"], //14
+	["U", "W", "I", "L", "R", "G"], //15
+	["P", "A", "C", "E", "M", "D"] //16
+  ]
+
 export default class board {
 	constructor() {
 		this._dice = [];
 		this._board = $(".board");
 		this._container = $(".container");
 		this._body = $("body");
+		this._config = this.generateConfig();
 		this.initDice()
 		this.renderDice();
+	}
+
+	generateConfig(){
+		let config_temp = "";
+		for (let i = 0; i < 16; i++) {
+
+			let random = Math.floor(Math.random() * 6);
+			let char = dices[i][random].toString()
+
+			config_temp += char;
+		}
+		return config_temp;
 	}
 
 	resetSelectedDice() {
@@ -24,6 +56,7 @@ export default class board {
 
 	refillBoard(){
 		this._dice = [];
+		this._config = this.generateConfig();
 		this.initDice();
 	}
 
@@ -56,7 +89,7 @@ export default class board {
 
 	initDice() {
 		for (let i = 0; i < 16; i++) {
-			let char = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+			let char = this._config[i];
 			let die_object = new die(i, char);
 			this._dice.push(die_object)
 		}
